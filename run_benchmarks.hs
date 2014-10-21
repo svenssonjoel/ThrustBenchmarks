8c31779c6f72d0988bc7771932a6e748ddc1479a
Joel Svensson <svenssonjoel@yahoo.se>
2014-10-21 15:46:40 -0400

    added run_bench

---
+++ run_benchmarks.hs
@@ -0,0 +1,56 @@
+#!/usr/bin/env runghc
+{-# LANGUAGE NamedFieldPuns #-}
+
+-- | This script runs all Harlan benchmarks.  It is based on a Haskell
+-- benchmarking framework called HSBencher.  Its main advantage is
+-- that it supports uploading of benchmark timings to a Google Fusion
+-- Table.
+-- Requires hsbencher >= 0.2
+
+import Control.Monad
+import Data.Maybe
+import qualified Data.ByteString.Char8 as B
+import System.Directory
+import System.FilePath
+import System.Exit
+import System.Environment (getArgs)
+import System.Process
+import GHC.Conc (getNumProcessors)
+import System.IO.Unsafe (unsafePerformIO)
+import Debug.Trace
+
+import HSBencher (defaultMainModifyConfig)
+import HSBencher.Backend.Fusion  (defaultFusionPlugin)
+import HSBencher.Backend.Dribble (defaultDribblePlugin)
+
+import HSBencher.Types
+import HSBencher.Internal.Logging (log)
+import HSBencher.Internal.MeasureProcess
+import HSBencher.Internal.Utils (runLogged, defaultTimeout)
+import Prelude hiding (log)
+--------------------------------------------------------------------------------
+
+main :: IO ()
+main = defaultMainModifyConfig myconf
+
+all_benchmarks :: [Benchmark DefaultParamMeaning]
+all_benchmarks =
+  [ (mkBenchmark "Reduce/Makefile" [elems] defaultCfgSpc)
+    { progname = Just "thrust-reduce" } 
+  | elems  <- [ show (2^n) | n <- [8..24] ] -- 256 to 32768
+  ] 
+
+-- | Default configuration space over which to vary settings:
+--   This is a combination of And/Or boolean operations, with the ability
+--   to set various environment and compile options.
+defaultCfgSpc = And []
+
+-- | Here we have the option of changing the HSBencher config
+myconf :: Config -> Config
+myconf conf =
+  conf
+   { benchlist = all_benchmarks
+   , plugIns   = [ SomePlugin defaultFusionPlugin,
+                   SomePlugin defaultDribblePlugin ]
+   }
+
