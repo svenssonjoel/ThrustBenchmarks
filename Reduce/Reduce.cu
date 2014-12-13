9da1780f1065473591d8fad26e0738259af9df86
Joel Svensson <svenssonjoel@yahoo.se>
2014-12-12 23:14:44 -0500

    more printing

--- Reduce/Reduce.cu
+++ Reduce/Reduce.cu
@@ -13,7 +13,8 @@
 
 int main(int argc, char **argv) { 
 
-  struct timeval tv1,tv2; 
+  struct timeval tv1,tv2;
+  struct timeval trans_t1, trans_t2;
   
   if (argc != 2) { 
     std::cout << "Incorrect args: needs size";
@@ -30,8 +31,10 @@ int main(int argc, char **argv) {
   thrust::generate(h_vec.begin(), h_vec.end(), rand);
 
   // copy host to device 
+  gettimeofday(&trans_t1, NULL); 
   thrust::device_vector<uint32_t> d_vec = h_vec;
-    
+  gettimeofday(&trans_t2, NULL); §    
+
   uint32_t x;
   gettimeofday(&tv1, NULL); 
   for (int i = 0; i < 1000; ++i) { 
@@ -44,8 +47,17 @@ int main(int argc, char **argv) {
     (double) (tv2.tv_usec - tv1.tv_usec) /1000000 + 
     (double) (tv2.tv_sec - tv1.tv_sec); 
 
+  double trans_secs =    
+    (double) (trans_t2.tv_usec - trans_t1.tv_usec) /1000000 + 
+    (double) (trans_t2.tv_sec - trans_t1.tv_sec); 
+
+  
   std::cout << "ELEMENTS_PROCESSED: " << size << "\n" ; 
   std::cout << "SELFTIMED: " << secs << "\n" ;
+  std::cout << "TRANSFER_TO_DEVICE: " << trans_secs << "\n";
+  std::cout << "BYTES_TO_DEVICE: " << size * sizeof(uint32_t) << "\n";
+  std::cout << "BYTES_FROM_DEVICE: " << 1 * sizeof(uint32_t) << "\n";
+
 
   return 0;
 }
