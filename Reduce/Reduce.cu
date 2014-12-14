2ba02b45391306c5821b0ff30add6fe1f9b09e66
Joel Svensson <svenssonjoel@yahoo.se>
2014-12-14 11:49:55 -0500

    typo fix

--- Reduce/Reduce.cu
+++ Reduce/Reduce.cu
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
   // copy host to device 
   gettimeofday(&trans_t1, NULL); 
   thrust::device_vector<uint32_t> d_vec = h_vec;
-  gettimeofday(&trans_t2, NULL); §    
+  gettimeofday(&trans_t2, NULL); 
 
   uint32_t x;
   gettimeofday(&tv1, NULL); 
